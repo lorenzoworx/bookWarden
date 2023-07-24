9e2702812fdc477e40206422dac7aa2fd6227843
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-07-24 18:04:15 +0100

    FIX linter errors

--- person.rb
+++ person.rb
@@ -2,23 +2,15 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(name = "unknown", age = nil, parent_permission = true)
+  def initialize(name: 'unknown', age: nil, parent_permission: true)
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
-
-  def age=(new_age)
-    @age = new_age
-  end
-
-  def can_use_services? 
-   return of_age? || @parent_permission
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   private
@@ -28,5 +20,6 @@ class Person
   end
 
   def generate_id
-    return Random.rand(1000..9999)
+    Random.rand(1000..9999)
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -2,13 +2,13 @@ require_relative 'person'
 
 class Teacher < Person
   attr_accessor :specialization
-  
-  def initialize(name = "Unknown", age = nil, parent_permission = true, specialization = nil)
+
+  def initialize(name: 'Unknown', age: nil, parent_permission: true, specialization: nil)
     super(name, age, parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
-    return true
+    true
   end
 end
